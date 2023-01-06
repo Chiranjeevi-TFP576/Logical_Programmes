c8b4c6bd32b8a0edf34aa5fa1c534c5bbb84eb4c
Chiranjeevi-TFP576 <p.chiranjeevi12345@gmail.com>
2023-01-06 20:05:39 +0530

    [chiru] Checking Febonacci_Number

---
+++ Fibonacci_Series.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_Programme_Day6
+{
+    public class Fibonacci_Series
+    {
+        public static void CheckFibonacci()//lets take 2->0+1=1,1+1=2
+        {
+            Console.WriteLine("Enter two number for checking Febonacci");
+            int n1, n2, count;
+            Console.WriteLine("Enter a N1 Value" );
+            n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a N1 Value");
+            n2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter febonacci count value");
+            count = Convert.ToInt32(Console.ReadLine());
+            for(int i=1;i<=count;i++)
+            {
+                int n3 = n1 + n2;
+                Console.WriteLine(""+n3);
+                n1 = n2;
+                n2 = n3;
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Logical_Programme_Day6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Logcals Programmes");
+            Fibonacci_Series.CheckFibonacci();
+        }
+    }
+}
